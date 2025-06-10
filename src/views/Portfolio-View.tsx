dcd3c6b2bce2f46b6981b0affd5a83cba5cfcfab
Siân <66971939+sian-codes@users.noreply.github.com>
2025-06-10 23:27:27 +0100

    First step to create portfolio view, add TODO's to plan out the work

---
+++ src/views/Portfolio-View.tsx
@@ -0,0 +1,11 @@
+import 'portfolio-view.css'
+
+const PortfolioView = () => {
+
+    // TODO: Create view of the portfolio into a grid format
+    // TODO: Include thumbnails of projects you've worked on and short descriptions
+    // TODO: Use React cards for reusability
+
+}
+
+return default PortfolioView
