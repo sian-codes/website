a55055e0f4dfd624e16723301babc1efe0a9eb47
Siân <66971939+sian-codes@users.noreply.github.com>
2025-06-25 22:13:35 +0100

    amend the theme to allow the body colour and switch background to change between themes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header.tsx";
 import ProfileView from "../src/views/ProfileView.tsx";
 import TechnologyView from "./views/TechnologyView.tsx";
 import PersonalPortfolioCard from "./components/PersonalPortfolioCard.tsx";
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import ReactSwitch from "react-switch";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMoon, faSunBright} from "@awesome.me/kit-459d1b3662/icons/classic/solid";
@@ -18,21 +18,25 @@ function App() {
         setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
     };
 
+    useEffect(() => {
+        document.body.id = theme;
+    }, [theme]);
+
     return (
   <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Header />
           <div className="switch">
-              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}
+              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} offColor={"#CAE8BD"}
                 checkedIcon={<div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
                       <FontAwesomeIcon icon={faMoon} style={{ color: "#fff", fontSize: 16 }} />
                   </div>
               }
-                           uncheckedIcon={
-                               <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
-                                   <FontAwesomeIcon icon={faSunBright} style={{ color: "#ffffff", fontSize: 16 }} />
-                               </div>
-                           }/>
+               uncheckedIcon={
+                   <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+                       <FontAwesomeIcon icon={faSunBright} style={{ color: "#ffffff", fontSize: 16 }} />
+                   </div>
+               }/>
           </div>
           <ProfileView />
             <TechnologyView />
